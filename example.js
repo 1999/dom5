c9ede0ddca46ffc213ba35ea9870b11d59d8f88d
Dmitry Sorin <info@staypositive.ru>
2012-06-18 10:40:20 +0400

    Update master

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-	var header = document.create("h1").addClass(["first", "head"]).plaintext("Header text");
-	var paraElem = document.create("p").attr({"id" : "para", "align" : "center"}).data("id", "smth").css("marginTop", "12px");
+    var header = document.create("h1").addClass(["first", "head"]).plaintext("Header text");
+    var paraElem = document.create("p").attr({"id" : "para", "align" : "center"}).data("id", "smth").css("marginTop", "12px").html("<span>some text</span>");
 
-	document.body.append([header, paraElem]);
+    document.body.append([header, paraElem]);
 }, false);
